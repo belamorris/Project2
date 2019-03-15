e20189650eb9b2ec1586e1396355b38605f04cd2
Dennis Ries <dtries@gmail.com>
2019-03-14 20:10:46 -0600

    fix push error

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -13,7 +13,9 @@ module.exports = function(app) {
 
   // Load example page and pass in an example by id
   app.get("/facesdb/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbFaces) {
+    db.Example.findOne({ where: { id: req.params.id } }).then(function(
+      dbFaces
+    ) {
       res.render("facesdb", {
         facesdb: dbFaces
       });
