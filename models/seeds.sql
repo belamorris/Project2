d01321d4c8fbc7b76f1c1f7d2db6104621d1a0ea
Dennis Ries <dtries@gmail.com>
2019-03-14 18:59:36 -0600

    Heroku Initial Commit

---
+++ models/seeds.sql
@@ -0,0 +1,29 @@
+USE dra3c5z6lyv8cu9x;
+
+INSERT INTO photolibrary(celebName, photoURL)
+VALUES ("Aurore", "https://images.pexels.com/photos/814052/pexels-photo-814052.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"),
+       ("Stephanie", "https://images.pexels.com/photos/712513/pexels-photo-712513.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"),
+       ("Katalina", "https://images.pexels.com/photos/789305/pexels-photo-789305.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"),
+       ("Luci","https://images.pexels.com/photos/886477/pexels-photo-886477.png?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"),
+       ("Afia","https://images.pexels.com/photos/1994818/pexels-photo-1994818.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"),
+       ("Rina","https://images.pexels.com/photos/1066116/pexels-photo-1066116.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"),
+       ("Monique","https://images.pexels.com/photos/1068207/pexels-photo-1068207.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"),
+       ("Therese","https://images.pexels.com/photos/1181742/pexels-photo-1181742.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"),
+       ("Akira","https://images.pexels.com/photos/977311/pexels-photo-977311.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"),
+       ("Asha","https://images.pexels.com/photos/1832959/pexels-photo-1832959.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"),
+       ("Malia","https://images.pexels.com/photos/1893198/pexels-photo-1893198.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"),
+       ("Jasmine","https://images.pexels.com/photos/1963075/pexels-photo-1963075.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"),
+       ("Alex","https://images.pexels.com/photos/247885/pexels-photo-247885.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"),
+       ("Chris","https://images.pexels.com/photos/1993094/pexels-photo-1993094.png?auto=compress&cs=tinysrgb&dpr=1&w=500"),
+       ("Marwin","https://images.pexels.com/photos/775358/pexels-photo-775358.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"),
+       ("Kasey","https://images.pexels.com/photos/935969/pexels-photo-935969.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"),
+       ("Parker","https://images.pexels.com/photos/1529350/pexels-photo-1529350.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"),
+       ("Berker","https://images.pexels.com/photos/1211605/pexels-photo-1211605.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"),
+       ("Gerard","https://images.pexels.com/photos/733505/pexels-photo-733505.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"),
+       ("Dorian","https://images.pexels.com/photos/1623536/pexels-photo-1623536.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"),
+       ("Kristian","https://images.pexels.com/photos/1812634/pexels-photo-1812634.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"),
+       ("Lucas","https://images.pexels.com/photos/1457824/pexels-photo-1457824.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"),
+       ("Kamon","https://images.pexels.com/photos/1153334/pexels-photo-1153334.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"),
+       ("Vinh","https://images.pexels.com/photos/1618592/pexels-photo-1618592.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500");
+       
+SELECT * FROM photolibrary;
