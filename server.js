97cdb859ab87c6b4d3185a6a3a1c47a705a8ad45
Dennis Ries <dtries@gmail.com>
2019-03-20 18:04:49 -0600

    Number Clients Connected

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 require("dotenv").config();
 var express = require("express");
 var app = express();
-var server = require('http').createServer(app);
-var io = require('socket.io')(server);
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
 var exphbs = require("express-handlebars");
-
 
 var db = require("./models");
 
@@ -38,14 +37,24 @@ if (process.env.NODE_ENV === "test") {
   syncOptions.force = true;
 }
 
-io.on('connection', () =>{
-  console.log('a user is connected')
-})
+var clients = 0;
+io.on("connection", function(socket) {
+  clients++;
+  io.sockets.emit("broadcast", {
+    description: clients + " clients connected!"
+  });
+  socket.on("disconnect", function() {
+    clients--;
+    io.sockets.emit("broadcast", {
+      description: clients + " clients connected!"
+    });
+  });
+});
 
 // Starting the server, syncing our models ------------------------------------/
 db.sequelize.sync(syncOptions).then(function() {
-  io.on('connection', () => { console.log("a user is connected") })
-  server.listen(PORT, function() {
+  // io.on('connection', () => { console.log("a user is connected") })
+  http.listen(PORT, function() {
     console.log(
       "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
       PORT,
